cea0d0d09ac887a8bb48fa9235e20dc4b5513c2c
laurielylourenco <laurielylourenco@gmail.com>
2023-10-16 23:03:58 -0300

    Classe conexao com banco de dados e usuario inicio

--- app/Controllers/Conn.php
+++ app/Controllers/Conn.php
@@ -1,14 +1,24 @@
 <?php
 
-class Conn {
-    private static $instance;
-    private $conn;
+namespace App\Controllers;
 
-    private function __construct() {
-        $db_host = 'localhost'; 
-        $db_name = 'agil_bot'; 
-        $db_user = 'postgres';  
-        $db_pass = 'lourenco';  
+
+use PDO;
+use PDOException;
+
+
+
+class Conn
+{
+    private static $instance;
+    public $conn;
+
+    private function __construct()
+    {
+        $db_host = 'localhost';
+        $db_name = 'agil_bot';
+        $db_user = 'postgres';
+        $db_pass = 'lourenco';
 
         try {
             $this->conn = new PDO("pgsql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
@@ -18,11 +28,16 @@ class Conn {
         }
     }
 
-    public static function getInstance() { // singleton 
+    public static function getInstance()
+    { // singleton 
         if (!isset(self::$instance)) {
             self::$instance = new self(); // cria uma instacia da classe atual
         }
         return self::$instance; // retorna instancia 
     }
 
+    public function prepare($sql)
+    {
+        return $this->conn->prepare($sql);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -1,2 +1,54 @@
 <?php
-?>
+
+namespace App\Models;
+
+use App\Controllers\Conn;
+
+
+class User
+{
+
+    private $nome;
+    private $senha;
+    private $login;
+    private $telefone;
+    private $tipo_usuario;
+    private $tipo_servico;
+
+
+    public function __construct(array $user)
+    {
+        $this->nome = $user['nome'];
+        $this->senha = $user['senha'];
+        $this->login = $user['login'];
+        $this->telefone = $user['telefone'];
+        $this->tipo_servico = $user['tipo_servico'];
+        $this->tipo_usuario = $user['tipo_usuario'];
+    }
+
+
+    public function createUser() : bool
+    {
+
+        $conn = Conn::getInstance();
+
+        $stmt =  $conn->prepare("
+            INSERT INTO  
+                usuario (login, senha, nome, telefone, tipo_usuario, tipo_servico) 
+            VALUES 
+                (:login, :senha, :nome, :telefone, :tipo_usuario, :tipo_servico)
+        ");
+
+        // Atribuição dos valores aos placeholders
+        $stmt->bindValue(':login', $this->login);
+        $stmt->bindValue(':senha', $this->senha);
+        $stmt->bindValue(':nome', $this->nome);
+        $stmt->bindValue(':telefone', $this->telefone);
+        $stmt->bindValue(':tipo_usuario', $this->tipo_usuario);  // Valor fixo '1' para tipo_usuario
+        $stmt->bindValue(':tipo_servico', $this->tipo_servico);  // Valor fixo '1' para tipo_servico
+
+        // Executa a consulta preparada
+        return  $stmt->execute();
+    }
+
+}
